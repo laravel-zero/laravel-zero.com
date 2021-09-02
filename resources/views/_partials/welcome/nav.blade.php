64306761e64eeabd50b62772b47e012e41178679
faissaloux <fwahabali@gmail.com>
2021-09-02 23:51:43 +0100

    use asset helper for images paths

--- resources/views/_partials/welcome/nav.blade.php
+++ resources/views/_partials/welcome/nav.blade.php
@@ -1,9 +1,9 @@
 <header class="flex items-center shadow-sm bg-white dark:bg-gray-900 py-4" role="banner">
     <div class="container flex items-center mx-auto px-6 space-x-4" >
         <div class="flex items-center">
-            <a href="/" title="{{ config('app.name')}} home" class="inline-flex items-center">
-                <img class="h-8 dark:hidden flex" loading="lazy" src="/assets/img/logo-dark.svg" alt="{{ config('app.name')}} logo" />
-                <img class="h-8 hidden dark:flex" loading="lazy" src="/assets/img/logo.svg" alt="{{ config('app.name')}} logo" />
+            <a href="/" title="{{ config('app.name') }} home" class="inline-flex items-center">
+                <img class="h-8 dark:hidden flex" loading="lazy" src="{{ asset('assets/img/logo-dark.svg') }}" alt="{{ config('app.name') }} logo" />
+                <img class="h-8 hidden dark:flex" loading="lazy" src="{{ asset('assets/img/logo.svg') }}" alt="{{ config('app.name') }} logo" />
             </a>
         </div>
 
