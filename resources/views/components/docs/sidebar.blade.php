c933668e22e58e7b83d8df6cdced10663f11c42b
Abdelhamid Errahmouni <abdelhamid.errahmouni@gmail.com>
2025-01-05 17:07:52 +0100

    finally fixed the stickiness of the sidebar h-full class of yhe parent was the problem.

--- resources/views/components/docs/sidebar.blade.php
+++ resources/views/components/docs/sidebar.blade.php
@@ -1,4 +1,4 @@
-<nav class="hidden lg:block mt-1 h-full lg:min-w-56 lg:min-h-screen"
+<nav class="hidden lg:block mt-1 lg:min-w-56 lg:min-h-screen"
 	:class="{ 'block lg:block': isOpen === true, 'hidden lg:block': isOpen === false }">
 	<div class="lg:sticky lg:w-56 xl:w-64 lg:px-0 lg:top-0 overflow-y-auto bg-white dark:bg-gray-900 scrollbar lg:h-screen fixed left-0 top-nav z-50 overflow-x-auto text-left px-4 h-screen-24 w-full"
 	x-ref="dialog" x-on:keydown.escape.window="close()">
