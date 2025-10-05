6868daebb4faef94d9ee0e1fd3b8ec505d1b9c28
Patricio <ijpatricio@gmail.com>
2025-10-05 21:26:36 +0100

    Add redirect for renamed documentation link.

--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
 
+Route::redirect('/docs/build-a-standalone-application', '/docs/distribute-as-phar');
+
 Route::get('/docs/{page?}', DocsController::class)->name('docs')->where('page', '.*');
